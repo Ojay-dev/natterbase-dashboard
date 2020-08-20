3915dbeaddd10e70c735944f55d7ef55f38a451a
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-08-20 15:27:46 +0100

    remove unwanted import

--- src/components/testActivity/testActivity.js
+++ src/components/testActivity/testActivity.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import { Task, ActivityTitle } from "../activity/activity";
 import styles from "./testActivity.module.scss";
