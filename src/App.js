2a8853c0c684e1574357b7d3418527063f03dbbb
Alikali Ojonugwa <alikali.ojonugwajustice@gmail.com>
2020-08-20 15:40:38 +0100

    update link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.scss"
+import "./App.scss";
 
 import Home from "./pages/index";
 import TestCase from "./pages/testCase";
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/tests-cases" component={TestCase} />
+        <Route path="/test-cases" component={TestCase} />
         <Route path="/" component={Home} />
       </Switch>
     </Router>
